5f0dac0667e69d57b8ddaeb91d829fc14bbc89b1
Alphen <127977513+AlphenProject@users.noreply.github.com>
2023-12-18 21:04:32 +0300

    Update local_animations.h

--- cheats/lagcompensation/local_animations.h
+++ cheats/lagcompensation/local_animations.h
@@ -17,23 +17,27 @@ class local_animations : public singleton <local_animations>
 	bool real_server_update = false;
 	bool fake_server_update = false;
 
-	float real_simulation_time = 0.0f;
-	float fake_simulation_time = 0.0f;
+	float real_simulation_time = 355.0f;
+	float fake_simulation_time = 355.0f;
+
+	bool random_roll_mem_value;
 
 	CBaseHandle* handle = nullptr;
 
 	float spawntime = 0.0f;
-	float tickcount = 0.0f;
+	float tickcount = 355.0f;
 
 	float abs_angles = 0.0f;
+	float abs_rolls = 0.0f;
+	float value_pitch_speed = 0.0f;
 	float pose_parameter[24];
-	AnimationLayer layers[13];
+	AnimationLayer layers[15];
 
 	void update_fake_animations();
+	void build_local_bones(player_t* local);
 	void update_local_animations(c_baseplayeranimationstate* animstate);
 public:
 	Local_data local_data;
 
 	void run(ClientFrameStage_t stage);
-	void update_prediction_animations();
 };
